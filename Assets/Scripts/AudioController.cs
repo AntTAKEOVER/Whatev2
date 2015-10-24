d5b12fc1ea72f0a9cfa7e27ae07f8fa8c3d2d2da
Wandermayer <chapmanz1700@my.uwstout.edu>
2015-10-24 02:25:44 -0500

    added audio, animation
    added assets

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+
+public class AudioController : MonoBehaviour {
+
+	public AudioMixerSnapshot Intense;
+	public AudioMixerSnapshot Calm;
+	private bool lowheath;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if
+	}
+
+	void ChangeMusic () {
+		Intense.TransitionTo(.01f);
+	}
+}
---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimation : MonoBehaviour {
+	private Animator character;
+
+	// Use this for initialization
+	void Start () {
+		character = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//character.SetFloat (Velocity, PlayerInput.moveVelocity.magnitude);
+	}
+}
